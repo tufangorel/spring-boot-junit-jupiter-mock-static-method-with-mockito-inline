66e8f90c2b6dd3244af92bb5af1621bcb92a7522
tufangorel <tufang@gmail.com>
2022-01-30 12:05:34 +0300

    Add private constructor to enforce noninstantiability for StringUtils class.

--- src/main/java/com/company/customerinfo/util/StringUtils.java
+++ src/main/java/com/company/customerinfo/util/StringUtils.java
@@ -2,6 +2,10 @@ package com.company.customerinfo.util;
 
 public class StringUtils {
 
+    private StringUtils() {
+        throw new AssertionError("shouldn't be instantiated");
+    }
+
     public static boolean isEmpty(final String str) {
         return str == null || str.length() == 0;
     }
